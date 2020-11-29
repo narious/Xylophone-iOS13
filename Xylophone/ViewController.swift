8d2e2f1479c3d90803aff9a55d0232f5331c47a2
Michael Dinh <tonkoalsce@gmail.com>
2020-11-30 00:17:21 +1100

    added button and sound

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        if let text = sender.titleLabel?.text {
+            print(text)
+        }
+        playSound()
+    }
     
-
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
